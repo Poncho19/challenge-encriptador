8a728c9c8379520947695c264f222d2e874ec53a
Alfonso Rafael Camacho Guerra <alfonsorafaelcamacho@gmail.com>
2024-03-14 13:31:07 -0600

    Primera etapa challenge

---
+++ encriptador.js
@@ -0,0 +1,24 @@
+//Se desarrolla un encriptador
+let palabraFrase;
+
+
+//palabraFrase = prompt("Dame la palabra o frase a encriptar: ");
+
+//console.log("La frase o palabra tiene " + (palabraFrase.length));
+
+/*palabraFrase = palabraFrase.replaceAll('e',"enter");
+palabraFrase = palabraFrase.replaceAll('i',"imes");
+palabraFrase = palabraFrase.replaceAll('a',"ai");
+palabraFrase = palabraFrase.replaceAll('o',"ober");
+palabraFrase = palabraFrase.replaceAll('u',"ufat");
+*/
+
+/*console.log(palabraFrase);
+
+palabraFrase = palabraFrase.replaceAll("enter",'e');
+palabraFrase = palabraFrase.replaceAll("imes",'i');
+palabraFrase = palabraFrase.replaceAll("ai",'a');
+palabraFrase = palabraFrase.replaceAll("ober",'o');
+palabraFrase = palabraFrase.replaceAll("ufat",'u');
+
+console.log(palabraFrase);*/
