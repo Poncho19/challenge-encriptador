c4b4d8dc1c2e595aa1977a5e1d26c39f0545f947
Alfonso Rafael Camacho Guerra <alfonsorafaelcamacho@gmail.com>
2024-03-17 20:03:49 -0600

    Version 2.5

--- encriptador.js
+++ encriptador.js
@@ -5,26 +5,43 @@ let palabraFrase;
 //Funcionalidad del boton encriptador
 function botonEncriptador(){
     let palabraEncriptada = "";
-    let palabraNormal = document.getElementById("area-1").value;
+    let palabraNormal = document.getElementById("area-1").value.toLowerCase();    
+    palabraNormal = eliminaAcentos(palabraNormal);
     palabraEncriptada = encriptar(palabraNormal);
     return document.getElementById("area-2").innerHTML = palabraEncriptada;
 }
 
 //Funcionalidad del boton desencriptar
 function botonDesencriptar(){
-    let palabraEncriptada = document.getElementById("area-1").value;
+    let palabraEncriptada = document.getElementById("area-1").value.toLowerCase();   
+    palabraEncriptada = eliminaAcentos(palabraEncriptada); 
     palabraNormal = desencriptar(palabraEncriptada);
     return document.getElementById("area-2").innerHTML = palabraNormal;
 }
 
+//Funcion que detecta acentos
+function eliminaAcentos(palabraAcentuada){    
+
+    palabraAcentuada = palabraAcentuada.replaceAll('é','e');
+    palabraAcentuada = palabraAcentuada.replaceAll('í','i');
+    palabraAcentuada = palabraAcentuada.replaceAll('á','a');
+    palabraAcentuada = palabraAcentuada.replaceAll('ó','o');
+    palabraAcentuada = palabraAcentuada.replaceAll('ú','u');
+    
+    return palabraAcentuada;
+}
+
 //Funcionalidad del boton copiar
 function botonCopiar(){ 
+    let textoCopiado = "";
     
     document.getElementById("area-2").select();
     document.execCommand('copy');
-    
-
-
+    document.getElementById("area-1").select();
+    document.execCommand('delete');
+    textoCopiado = document.getElementById("area-2").value;
+    console.log(textoCopiado);
+    window.getSelection().removeAllRanges();
     return;
 }
 
